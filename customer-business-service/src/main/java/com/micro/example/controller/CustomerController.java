7ac6093744f01fd0d40416544325c157c014e6ce
maoyc <maoyc@polysoft.com.cn>
2018-04-17 16:52:02 +0800

    customer

--- customer-business-service/src/main/java/com/micro/example/controller/CustomerController.java
+++ customer-business-service/src/main/java/com/micro/example/controller/CustomerController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
 /**
  * Created by mycge on 2018/4/16.
  */
